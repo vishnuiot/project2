e9e2da648e2ff337f80bdc4f2bb2e0e2b607bc6a
vishnuiot <vishnuprasad.iit@gmail.com>
2022-10-18 10:32:49 +0200

    change

--- gitupload.sh
+++ gitupload.sh
@@ -4,5 +4,5 @@ git init
 git commit -m "change"
 git branch -M main
 git remote add origin https://github.com/vishnuiot/project2.git
-git pull https://github.com/vishnuiot/project2.git
+git merge
 git push -u origin main
